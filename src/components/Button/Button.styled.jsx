8c6b0bb9cc749739d2dae470fedacb337aae34b5
Evgeniy <evgnec@gmail.com>
2023-06-25 11:08:04 +0300

    add b

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -1,14 +1,16 @@
 import styled from '@emotion/styled';
 
 export const Btn = styled.button`
-  padding: 8px 16px;
-  margin: 0 auto;
-  border-radius: 2px;
-  background-color: #3f51b5;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 24px;
+  padding: 8px 0;
+  border-radius: 25px;
+  background-color: var(--theme-color);
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
-  display: inline-block;
-  color: #fff;
+  color: var(--bg-color);
   border: 0;
   text-decoration: none;
   cursor: pointer;
@@ -17,12 +19,10 @@ export const Btn = styled.button`
   line-height: 24px;
   font-style: normal;
   font-weight: 500;
-  min-width: 180px;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  min-width: 150px;
 
   :hover,
   :focus {
-    background-color: #303f9f;
+  background-color: var(--theme-color-hover);
   }
 `;
